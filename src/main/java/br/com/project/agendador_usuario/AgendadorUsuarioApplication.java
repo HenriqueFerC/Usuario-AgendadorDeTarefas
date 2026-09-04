81a89252eeb6c6e3ddcef7cd102fcabd5649f464
HenriqueFerC <henriqueferreiracardoso179411@hotmail.com>
2026-09-04 17:30:12 -0300

    commit inicial

---
+++ src/main/java/br/com/project/agendador_usuario/AgendadorUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.agendador_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/agendador_usuario/AgendadorUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.agendador_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
